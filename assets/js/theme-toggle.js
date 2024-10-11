ae7fa4468acc35d98fd6349ddc2cec3402bd27d4
Binit Kumar <binit-kumar@users.noreply.github.com>
2024-10-11 14:58:08 -0500

    cosmetic changes for education description and dynamic text.

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const feedbackInputs = document.querySelectorAll('.feedback-input');
   const submitButton = document.querySelector('[type="submit"]');
   const dynamicTextElement = document.getElementById('dynamic-text');
-  const roles = ["Full Stack Developer, ", "Cloud Engineer, ", "Principal Software Engineer, "];
+  const roles = ["Full Stack Developer,", "Cloud Engineer,", "Principal Software Engineer,"];
   const svgLabels = document.querySelectorAll('.svg-label');
   const plusIcon = document.querySelector('.plus-icon');
   const minusIcon = document.querySelector('.minus-icon');
